5cde9e64daa57a105df86c9a92419dcafba65e07
ucssoftware <55113624+ucssoftware@users.noreply.github.com>
2019-09-10 18:38:10 +0200

    Add files via upload

--- js/navbar.js
+++ js/navbar.js
@@ -7,7 +7,8 @@ var html = '  <nav class="blue darken-4" role="navigation">\
   </ul>\
   \
   <ul id="nav-mobile" class="sidenav">\
-    <li><a href="#">Navbar Link</a></li>\
+    <li><a href="index.html">Home</a></li>\
+    <li><a href="games.html">Games</a></li>\
   </ul>\
   <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>\
 </div>\
