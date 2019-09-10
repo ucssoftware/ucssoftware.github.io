95c02d0071fd61f31e0377df9e1683f857d9cd75
ucssoftware <55113624+ucssoftware@users.noreply.github.com>
2019-09-10 18:35:50 +0200

    Add files via upload

---
+++ js/navbar.js
@@ -0,0 +1,16 @@
+var html = '  <nav class="blue darken-4" role="navigation">\
+<div class="nav-wrapper container"><a id="logo-container" href="index.html" class="brand-logo"><img src="UCS1.svg" height="auto" width="100">\
+</a>\
+  <ul class="right hide-on-med-and-down">\
+    <li><a href="index.html">Home</a></li>\
+    <li><a href="games.html">Games</a></li>\
+  </ul>\
+  \
+  <ul id="nav-mobile" class="sidenav">\
+    <li><a href="#">Navbar Link</a></li>\
+  </ul>\
+  <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>\
+</div>\
+</nav>';
+
+document.getElementById('nav').innerHTML = html;
